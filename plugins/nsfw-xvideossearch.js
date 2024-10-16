22b8f7a3249666d71c54b38fa985f15321668df5
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-10-16 17:12:08 +0200

    Add files via upload

---
+++ plugins/nsfw-xvideossearch.js
@@ -0,0 +1,26 @@
+//XVIDEOS -  SEARCH
+import fg from 'api-dylux'
+
+let handler = async (m, { conn, command, args, text, usedPrefix }) => {
+if (!global.db.data.chats[m.chat].nsfw) return conn.reply(m.chat, `🚩 El grupo no admite contenido *Nsfw.*\n\n> Para activarlo un *Administrador* debe usar el comando *on nsfw*`, m, rcanal)
+if (!text) return conn.reply(m.chat, 'ingresa el texto de lo que quieres buscar', m)
+
+try {
+let res = await fg.xvideosSearch(text)
+let img = res[0].thumb
+let txt = 'XVIDEOS  -  SEARCH '
+for (let i = 0; i < res.length; i++) {
+txt += `\n\n`
+txt += `*Nro* : ${i + 1}\n`
+txt += `*Titulo* : ${res[i].title}\n`
+txt += `*Duracion* : ${res[i].duration}\n`
+txt += `*Url* : ${res[i].url}`
+}
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
+} catch (error) {
+console.log(error)
+}}
+
+handler.command = ['xvideossearch']
+handler.tags = ['nsfw'] 
+export default handler
